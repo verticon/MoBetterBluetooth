c40923ab28e2f9c8d5e94564c6eacdc14ad52528
Robert Vaessen <robert@rvaessen.com>
2017-04-01 11:05:15 -0400

    Made the subscriptions persistable

--- MoBetterBluetooth/CentralManager/Subscriptions.swift
+++ MoBetterBluetooth/CentralManager/Subscriptions.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 import CoreBluetooth
+import VerticonsToolbox
 
 extension CentralManager {
 
-    public struct Identifier {
+    public struct Identifier : Encodable  {
         public let uuid: CBUUID
         public let name: String?
         
@@ -19,9 +20,25 @@ extension CentralManager {
             self.uuid = uuid
             self.name = name ?? nameForWellKnownUuid(uuid)
         }
+        
+        public init?(_ properties: Encodable.Properties?) {
+            guard let properties = properties else { return nil }
+
+            if  let name = properties["name"] as? String,
+                let uuid = properties["uuid"] as? String {
+                self.name = name
+                self.uuid = CBUUID(string: uuid)
+            } else {
+                return nil
+            }
+        }
+
+        public func encode() -> Encodable.Properties {
+            return ["name": name ?? "", "uuid": uuid.uuidString]
+        }
     }
 
-    public struct CharacteristicSubscription {
+    public struct CharacteristicSubscription : Encodable {
         public let id: Identifier
         public let discoverDescriptors: Bool
 
@@ -29,15 +46,47 @@ extension CentralManager {
             self.id = id
             self.discoverDescriptors = discoverDescriptors
         }
+
+        public init?(_ properties: Encodable.Properties?) {
+            guard let properties = properties else { return nil }
+
+            if  let id = Identifier(properties["id"] as? Encodable.Properties),
+                let discover = properties["discover"] as? Bool {
+                self.id = id
+                self.discoverDescriptors = discover
+            } else {
+                return nil
+            }
+        }
+        
+        public func encode() -> Encodable.Properties {
+            return ["id": id.encode(), "discover": discoverDescriptors]
+        }
     }
 
-    public struct ServiceSubscription {
+    public struct ServiceSubscription : Encodable {
         public let id: Identifier
         public var characteristics: [CharacteristicSubscription] // If empty then all of the service's characteristics and descriptors will be discovered
         
         public init(id: Identifier, characteristics: [CharacteristicSubscription] = []) {
             self.id = id
             self.characteristics = characteristics
+        }
+
+        public init?(_ properties: Encodable.Properties?) {
+            guard let properties = properties else { return nil }
+
+            if  let id = Identifier(properties["id"] as? Encodable.Properties),
+                let characteristics = properties["characteristics"] as? [Encodable.Properties] {
+                self.id = id
+                self.characteristics = characteristics.decode(type: CharacteristicSubscription.self)
+            } else {
+                return nil
+            }
+        }
+        
+        public func encode() -> Encodable.Properties {
+            return ["id": id.encode(), "characteristics": characteristics.encode()]
         }
 
         subscript(characteristicId: CBUUID) -> CharacteristicSubscription? {
@@ -64,13 +113,31 @@ extension CentralManager {
         }
     }
 
-    public struct PeripheralSubscription {
+    public struct PeripheralSubscription : Encodable {
+        public let name: String
         public var services: [ServiceSubscription] // If empty then all of the peripheral's services, characteristics and descriptors will be discovered
    
-        public init(services: [ServiceSubscription] = []) {
+        public init(name: String, services: [ServiceSubscription] = []) {
+            self.name = name
             self.services = services
         }
-
+        
+        public init?(_ properties: Encodable.Properties?) {
+            guard let properties = properties else { return nil }
+            
+            if  let name = properties["name"] as? String,
+                let services = properties["services"] as? [Encodable.Properties] {
+                self.name = name
+                self.services = services.decode(type: ServiceSubscription.self)
+            } else {
+                return nil
+            }
+        }
+        
+        public func encode() -> Encodable.Properties {
+            return ["name": name, "services": services.encode()]
+        }
+        
         subscript(serviceId: CBUUID) -> ServiceSubscription? {
             let result = services.filter() { $0.id.uuid == serviceId }
             return result.count == 1 ? result[0] : nil
@@ -109,5 +176,17 @@ extension CentralManager {
 extension CentralManager.Identifier : Equatable {
     public static func == (lhs: CentralManager.Identifier, rhs: CentralManager.Identifier) -> Bool {
         return lhs.uuid == rhs.uuid
+    }
+}
+
+extension Array where Element == Encodable.Properties {
+    func decode<T:Encodable>(type: T.Type) -> [T] {
+        return flatMap{ T($0) }
+    }
+}
+
+extension Array where Element : Encodable {
+    func encode() -> [Encodable.Properties] {
+        return map{ $0.encode() }
     }
 }
